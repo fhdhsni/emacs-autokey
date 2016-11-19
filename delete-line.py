955a52c7f803b3af376db5ce0892eabbc9429033
Farhad hsni <farhad.hsni@gmail.com>
2016-11-19 21:22:25 +0330

    Adding more

---
+++ delete-line.py
@@ -0,0 +1,4 @@
+# Enter script code
+keyboard.send_key("<home>")
+keyboard.send_keys("<shift>+<end>")
+keyboard.send_key("<delete>")
---
+++ open-line.py
@@ -0,0 +1,3 @@
+# Enter script code
+keyboard.send_key("<enter>")
+keyboard.send_key("<up>")
